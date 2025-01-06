13208195152f091e8a1ffb50177a3ee8110e2a4b
Abdeldjalil <pointbreak691227@gmail.com>
2025-01-06 23:43:13 +0100

    remove naive from dp

--- LCS/LCS.c
+++ LCS/LCS.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include "LCS.h"
 
-// LCS naive
+/* // LCS naive
 int lcs_naive(const char *X, const char *Y, int m, int n) {
     if (m == 0 || n == 0)
         return 0;
@@ -13,16 +13,19 @@ int lcs_naive(const char *X, const char *Y, int m, int n) {
     else
         return (lcs_naive(X, Y, m, n - 1) > lcs_naive(X, Y, m - 1, n)) ? lcs_naive(X, Y, m, n - 1) : lcs_naive(X, Y, m - 1, n);
 }
-
+ */
 // Fonction pour récupérer la LCS à partir de la table de programmation dynamique
-char* get_lcs(const char *X, const char *Y, int m, int n) {
+char *get_lcs(const char *X, const char *Y, int m, int n)
+{
 
     int **L = (int **)malloc((m + 1) * sizeof(int *));
     for (int i = 0; i <= m; i++)
         L[i] = (int *)calloc(n + 1, sizeof(int));
 
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
             if (X[i - 1] == Y[j - 1])
                 L[i][j] = L[i - 1][j - 1] + 1;
             else
@@ -34,15 +37,21 @@ char* get_lcs(const char *X, const char *Y, int m, int n) {
     lcs[index] = '\0';
 
     int i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (X[i - 1] == Y[j - 1]) {
+    while (i > 0 && j > 0)
+    {
+        if (X[i - 1] == Y[j - 1])
+        {
             lcs[index - 1] = X[i - 1];
             i--;
             j--;
             index--;
-        } else if (L[i - 1][j] > L[i][j - 1]) {
+        }
+        else if (L[i - 1][j] > L[i][j - 1])
+        {
             i--;
-        } else {
+        }
+        else
+        {
             j--;
         }
     }
@@ -54,14 +63,17 @@ char* get_lcs(const char *X, const char *Y, int m, int n) {
     return lcs;
 }
 
-// Fonction pour calculer LCS 
-int lcs_dp(const char *X, const char *Y, int m, int n) {
+// Fonction pour calculer LCS
+int lcs_dp(const char *X, const char *Y, int m, int n)
+{
     int **L = (int **)malloc((m + 1) * sizeof(int *));
     for (int i = 0; i <= m; i++)
         L[i] = (int *)calloc(n + 1, sizeof(int));
 
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
             if (X[i - 1] == Y[j - 1])
                 L[i][j] = L[i - 1][j - 1] + 1;
             else
@@ -78,25 +90,30 @@ int lcs_dp(const char *X, const char *Y, int m, int n) {
 }
 
 // Tests pour des tailles différentes
-void complexite_lcs() {
+void complexite_lcs()
+{
     srand(time(NULL)); // Initialiser le générateur de nombres aléatoires
     FILE *f = fopen("lcs_results.csv", "w");
-    if (f == NULL) {
+    if (f == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
 
     fprintf(f, "i,Length_LCS,time\n");
-    for (int i = 10; i <= 1000000; i += 10) {
+    for (int i = 10; i <= 1000000; i += 10)
+    {
         char *X = (char *)malloc((i / 2 + 1) * sizeof(char));
         char *Y = (char *)malloc((i + 1) * sizeof(char));
 
-        for (int k = 0; k < i / 2; k++) {
+        for (int k = 0; k < i / 2; k++)
+        {
             X[k] = 'A' + (rand() % 26);
         }
         X[i / 2] = '\0';
 
-        for (int k = 0; k < i; k++) {
+        for (int k = 0; k < i; k++)
+        {
             Y[k] = 'A' + (rand() % 26);
         }
         Y[i] = '\0';
@@ -117,66 +134,54 @@ void complexite_lcs() {
 }
 
 // Menu principal
-void display_menu_lcs() {
+void display_menu_lcs()
+{
     int choice;
-    do {
+    do
+    {
         printf("\nMenu:\n");
-        printf("1. LCS naive\n");
-        printf("2. LCS dynamique\n");
-        printf("3. Essaies LCS dynamique pour complexite experimentale\n");
-        printf("4. Exit\n");
+        printf("1. Saisir des chaines\n");
+        printf("2. Essaies LCS dynamique pour complexite experimentale\n");
+        printf("3. Exit\n");
         printf("Entrer un choix: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1: {
-                char X[1000], Y[1000];
-                printf("Enter the first string: ");
-                scanf("%s", X);
+        switch (choice)
+        {
+        case 1:
+        {
+            char X[1000], Y[1000];
+            char *r;
+            printf("Enter the first string: ");
+            scanf("%s", X);
 
-                printf("Enter the second string: ");
-                scanf("%s", Y);
+            printf("Enter the second string: ");
+            scanf("%s", Y);
 
-                int m = strlen(X);
-                int n = strlen(Y);
+            int m = strlen(X);
+            int n = strlen(Y);
 
-                printf("\nFirst string (X): %s\n", X);
-                printf("Second string (Y): %s\n", Y);
-                printf("LCS: %d\n", lcs_naive(X, Y, m, n));
-                break;
-            }
-            case 2: {
-                char X[1000], Y[1000];
-                char *r;
-                printf("Enter the first string: ");
-                scanf("%s", X);
+            printf("\nFirst string (X): %s\n", X);
+            printf("Second string (Y): %s\n", Y);
 
-                printf("Enter the second string: ");
-                scanf("%s", Y);
-
-                int m = strlen(X);
-                int n = strlen(Y);
-
-                printf("\nFirst string (X): %s\n", X);
-                printf("Second string (Y): %s\n", Y);
-                
-                r = get_lcs(X, Y, m, n);
-                printf("\nLCS: %s\n", r);
-                break;
-            }
-            case 3:
-                complexite_lcs();
-                break;
-            case 4:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
+            r = get_lcs(X, Y, m, n);
+            printf("\nLCS: %s\n", r);
+            break;
         }
-    } while (choice != 4);
+        case 2:
+            complexite_lcs();
+            break;
+        case 3:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 3);
 }
 
-int main() {
+/* int main()
+{
     display_menu_lcs();
     return 0;
-}
+} */
